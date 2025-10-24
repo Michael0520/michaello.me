91fed47ff63add4d86ad18e6797c8d428ff9cfea
Michael Lo <michael860520@gmail.com>
2025-10-24 22:02:48 +0800

    fix(portfolio): remove standalone mode (incompatible with Nx)

--- apps/portfolio/next.config.mjs
+++ apps/portfolio/next.config.mjs
@@ -21,7 +21,6 @@ const nextConfig = {
   transpilePackages: ['next-mdx-remote', '@milo-me/blog-metadata', '@milo-me/site-config', 'fumadocs-core', 'fumadocs-mdx'],
   devIndicators: false,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  output: 'standalone',
   experimental: {
     externalDir: true,
   },
